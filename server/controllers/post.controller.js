ac91094f85b9fc33f1bf184c49c3e88b72059ba6
magino <magino.cochet@gmail.com>
2020-02-05 16:20:58 +0100

    server api

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,87 @@
+const Post = require('../models/post');
+const cuid = require('cuid');
+const slug = require('limax');
+const sanitizeHtml = require('sanitize-html');
+
+/**
+ * Get all posts
+ * @param req
+ * @param res
+ * @returns void
+ */
+getPosts = async (req, res) => {
+  Post.find().sort('-dateAdded').exec((err, posts) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ posts });
+  });
+};
+
+/**
+ * Save a post
+ * @param req
+ * @param res
+ * @returns void
+ */
+addPost = async (req, res) => {
+  if (!req.body.post.name || !req.body.post.title || !req.body.post.content) {
+    res.status(403).end();
+  }
+
+  const newPost = new Post(req.body.post);
+
+  // Let's sanitize inputs
+  newPost.title = sanitizeHtml(newPost.title);
+  newPost.name = sanitizeHtml(newPost.name);
+  newPost.content = sanitizeHtml(newPost.content);
+
+  newPost.slug = slug(newPost.title.toLowerCase(), { lowercase: true });
+  newPost.cuid = cuid();
+  newPost.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ post: saved });
+  });
+};
+
+/**
+ * Get a single post
+ * @param req
+ * @param res
+ * @returns void
+ */
+getPost = async (req, res) => {
+  Post.findOne({ cuid: req.params.cuid }).exec((err, post) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ post });
+  });
+};
+
+/**
+ * Delete a post
+ * @param req
+ * @param res
+ * @returns void
+ */
+deletePost = async (req, res) => {
+  Post.findOne({ cuid: req.params.cuid }).exec((err, post) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    post.remove(() => {
+      res.status(200).end();
+    });
+  });
+};
+
+module.exports = {
+  getPosts,
+  addPost,
+  getPost,
+  deletePost
+};
---
+++ server/routes/post.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const PostController = require('../controllers/post.controller');
+
+// Get all Posts
+router.route('/posts').get(PostController.getPosts);
+
+// Get one post by cuid
+router.route('/posts/:cuid').get(PostController.getPost);
+
+// Add a new Post
+router.route('/posts').post(PostController.addPost);
+
+// Delete a post by cuid
+router.route('/posts/:cuid').delete(PostController.deletePost);
+
+module.exports = router;
